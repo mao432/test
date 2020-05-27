9559aadc0d70ca73f94e92467f6ad1fc181da8b0
mao432 <66034436+mao432@users.noreply.github.com>
2020-05-27 21:46:01 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+    bot.user.setActivity("^^Aide | En cour de développement");
+    console.log("Le bot est en ligne !!");
+});
+
+bot.login(process.env.TOKEN);
